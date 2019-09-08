44a3f9624f0571b4aed82de128fcfaa9c4fcb1b5
MvGool <54936422+MvGool@users.noreply.github.com>
2019-09-08 14:56:47 +0200

    Updated multiplication
    forgot to save the changes

--- Multiplication.py
+++ Multiplication.py
@@ -1,8 +1,8 @@
 # declare variables
-integer1 = [4,2,8,2,3,4,5,7,8,8,3,4,6,3,5,6,4,3,5,6]
-integer2 = [6,4,0,9,4,6,7,8,6,4,7,5,7,5,3,6,7,8,6,1]
-radix1 = 10
-radix2 = 10
+integer1 = [5,6,14,1]#[4,2,8,2,3,4,5,7,8,8,3,4,6,3,5,6,4,3,5,6,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
+integer2 = [0,0,3,11]#[6,4,0,9,4,6,7,8,6,4,7,5,7,5,3,6,7,8,6,1,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0]
+radix1 = 16
+radix2 = 16
 positive1 = True
 positive2 = True
 
@@ -15,11 +15,9 @@ def multiplication(integer1, interger2, radix1, radix2, positive1, positive2):
     digit2 = integer2[-(i+1)]
     for j in range(0, n):
       digit1 = integer1[-(j+1)]
-      outputNumber = digit1*digit2*10**(i+j)
-      outputNumberString = str(outputNumber)
-      numLength = len(outputNumberString)
-      for k in range(1, numLength+1):
-        output[-k] += int(outputNumberString[-k])
+      outputNumber = digit1*digit2
+      output[-(1+i+j)] += outputNumber
+  print(output)
 
   for i in range(1, len(output)+1):
     if output[-i] >= radix1:
@@ -27,6 +25,6 @@ def multiplication(integer1, interger2, radix1, radix2, positive1, positive2):
       output[-i] = output[-i]%radix1
       output[-(i+1)] += carry
 
-  print(output)
+  print(output, 2*n)
 
 multiplication(integer1, integer2, radix1, radix2, positive1, positive2)

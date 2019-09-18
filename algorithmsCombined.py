acd9b14d7a90177c2e93da136045bbb3f4fcd567
smartiej <54936446+smartiej@users.noreply.github.com>
2019-09-18 15:32:41 +0200

    All algorithms

--- algorithmsCombined.py
+++ algorithmsCombined.py
@@ -1,117 +1,123 @@
-# listX = [13, 15, 9, 14, 7, 6, 13, 1, 1, 3, 8, 9, 5, 8, 2, 1, 12, 5, 6, 7]
-# listY = [6, 10, 2, 0, 11, 1, 8, 8, 6, 7, 5, 10, 11, 3, 9, 14, 1, 7, 10, 2]
-listX = [1, 3, 4]
-listY = [2, 3]
-positiveX = False
-positiveY = True
-radix = 10
-listXY = listX, listY
-
-maxLen = max(map(len, listXY))
-minLen = min(map(len, listXY))
-
+# All algorithms combined
 def addition(inputX, positiveX, inputY, positiveY, b):
-    if (positiveX == False) and (positiveY == False):
-        positiveX = True
-        positiveY = True
-        output = subtraction(inputX, positiveY, inputY, positiveX, b)
-        return 'negative', output
-
-    # if X is a negative number, then subtract Y from X
-    elif (positiveX == False) and (positiveY == True):
-        positiveX = True
-        output = subtraction(inputX, positiveY, inputY, positiveX, b)
-        return 'negative', output
-    # if Y is a negative number, then subtract X from Y 
-    elif (positiveY == False) and (positiveX == True):
-        positiveY = True
-        output = subtraction(inputX, positiveX, inputY, positiveY, b) 
-        return output
-
-    # if both X and Y are positive, then add both digits
-    if (positiveX == True) and (positiveY == True):
-        # add digits 
-        for digits in listXY:
-            while len(digits) < maxLen:
-                digits.insert(0, 0)
-        
-        # sum the digits from listX with listY according to index in each list
-        sumXY = [sum(x) for x in zip(*listXY)]
-        
-        # calculate if there is a carry for each element and then add a zero to the end, to make sure the carry goes to the digit on the right side of the element
-        for i in range(maxLen):
-            carryXY = [item // radix for item in sumXY]
-            carryXY.append(0)
-            
-        sumcarryXY = sumXY, carryXY
-        maxLenSum = max(map(len, sumcarryXY))
-
-        # make sure that the length of both sumXY and carryXY is the same 
-        for digits in sumcarryXY:
-            while len(digits) < maxLenSum:
-                digits.insert(0, 0)
-
-        # add the carry stored in carryXY to sumXY
-        addcarryXY = [sum(x) for x in zip(*sumcarryXY)]
-        # now only keep the mod of the digit, since the carry is already added to the digit on the right
-        output = [item % radix for item in addcarryXY]
-            
-        return output
-
-# def subtraction(x, positiveX, y, positiveY, b)
-#     if (positiveY == False):
-#         positiveY = True
-#         addition(x, positiveX, y, positiveY, b) 
-    
-
-def subtraction(inputX, positiveX, inputY, positiveY, b):
-    result = []
     lenX = len(inputX)
     lenY = len(inputY)
-    reverseX = inputX[::-1]
-    reverseY = inputY[::-1] #reverse the list
+    inputXY = inputX, inputY
+    maxLen = max(map(len, inputXY))
+
+    # add digits 
+    for digits in inputXY:
+        while len(digits) <= maxLen:
+            digits.insert(0, 0)
 
     if (positiveX == False) and (positiveY == False):
-        positiveX = True
-        output = addition(inputX, positiveX, inputY, positiveY, b)
-        return 'negative', output
+        # set positive indicator
+        positiveOutput = False
+        # run addition
+        output = addition(inputX, True, inputY, True, b)[1]
+
     elif (positiveX == True) and (positiveY == False):
-        positiveY = True
-        output = addition(inputX, positiveX, inputY, positiveY, b) 
-        return output 
+        # run subtraction
+        [positiveOutput, output] = subtraction(inputX, True, inputY, True, b)
+
     elif (positiveX == False) and (positiveY == True):
-        positiveX = True 
-        output = addition(inputX, positiveX, inputY, positiveY, b)
-        return 'negative', output
-    else:
-        for i in range(minLen,maxLen):
-            if lenX < lenY: reverseX.append(0)
-            if lenX > lenY: reverseY.append(0) #fill up with zeros to balance length
+        # run subtraction
+        [positiveOutput, output] = subtraction(inputY, True, inputX, True, b)
+
+    elif (positiveX == True) and (positiveY == True):
+        # set positive indicator
+        positiveOutput = True
         
-        inputX = reverseX[::-1]
-        inputY = reverseY[::-1]
+        # sum the digits from listX with listY according to index in each list
+        sumXY = [sum(x) for x in zip(*inputXY)]
+        output = sumXY.copy()
+        for i in range(1, len(output)):
+            rem, carry = 0, 0
+            if output[-i] >= b:
+                carry = output[-i] // b
+                output[-i] = output[-i] % b
+                output[-(i+1)] += carry
 
-        for i in range(0,maxLen):
-            digit = inputX[i] - inputY[i]
-            result.append(digit)
+    i = 0
+    while i < (len(output) - 1):
+        if (output[i] == 0):
+            output.pop(0)
+            continue
+        break
 
-            if b != 0:
-                result[i] %= b
-                
-            if inputX[i] < inputY[i]:
-                result[i-1] = result[i-1] - 1
+    return [positiveOutput, output]
 
-        p = result
 
-        # remove preceding zeroes
-        while p[0] == 0 and not len(p) == 1:
-            p = p[1:]
+def subtraction(inputX, positiveX, inputY, positiveY, b):
+    lenX = len(inputX)
+    lenY = len(inputY)
+    inputXY = inputX, inputY
+    maxLen = max(map(len, inputXY))
 
-        return p
+    # add digits 
+    for digits in inputXY:
+        while len(digits) < maxLen:
+            digits.insert(0, 0)
+
+    if (positiveX == False) and (positiveY == False):
+        # run addition
+        [positiveOutput, output] = subtraction(inputY, True, inputX, True, b)
+
+    elif (positiveX == True) and (positiveY == False):
+        # run subtraction
+        [positiveOutput, output] = addition(inputX, True, inputY, True, b)
+
+    elif (positiveX == False) and (positiveY == True):
+        # run subtraction
+        [positiveOutput, output] = addition(inputX, False, inputY, False, b)
+
+    elif (positiveX == True) and (positiveY == True):
+        # check which number is bigger
+        for i in range(maxLen):
+            if (inputX[i] > inputY[i]):
+                positiveOutput = True
+            elif (inputX[i] < inputY[i]):
+                inputTemp= inputX.copy()
+                inputX = inputY.copy()
+                inputY = inputTemp.copy()
+                inputXY = inputX, inputY
+                positiveOutput = False
+            elif (inputX[i] == inputY[i]):
+                if (i == maxLen - 1):
+                    return [True, [0]]
+                continue
+            break
+        
+        # sum the digits from listX with listY according to index in each list
+        diffXY = [0] * maxLen
+        for i in range(maxLen):
+            diffXY[i] = inputX[i] - inputY[i]
+        output = diffXY.copy()
+
+        for i in range(1, len(output)):
+            rem = 0
+            if output[-i] < 0:
+                output[-(i+1)] -= 1
+                rem = output[-i] + b
+                output[-i] = rem
+
+    i = 0
+    while i < (len(output) - 1):
+        if (output[i] == 0):
+            output.pop(0)
+            continue
+        break
+
+    return [positiveOutput, output]
 
 
 def multiplication(inputX, positiveX, inputY, positiveY, b):
-    for digits in listXY:
+    inputXY = inputX, inputY
+    maxLen = max(map(len, inputXY))
+    minLen = min(map(len, inputXY))
+    positiveOutput = not(positiveX ^ positiveY)
+
+    for digits in inputXY:
         while len(digits) < maxLen:
             digits.insert(0, 0)
 
@@ -124,14 +130,214 @@ def multiplication(inputX, positiveX, inputY, positiveY, b):
             digit1 = inputX[-(j+1)]
             outputNumber = digit1 * digit2
             output[-(1+i+j)] += outputNumber
-    print(output)
 
     for i in range(1, len(output)+1):
         if output[-i] >= b:
             carry = output[-i] // b
             output[-i] = output[-i] % b
             output[-(i+1)] += carry
+    
+    i = 0
+    while i < (len(output) - 1):
+        if (output[i] == 0):
+            output.pop(0)
+            continue
+        break
 
-    print(output, 2*n)
+    return [positiveOutput, output]
 
-multiplication(listX, positiveX, listY, positiveY, radix)
+
+def karatsuba(inputX, positiveX, inputY, positiveY, b):
+    positiveOutput = not (positiveX ^ positiveY)
+
+    i = 0;
+    while i < (len(inputX)-1):
+        if (inputX[i] == 0):
+            inputX.pop(0)
+            continue
+        break
+
+    i = 0;
+    while i < (len(inputY)-1):
+        if (inputY[i] == 0):
+            inputY.pop(0)
+            continue
+        break
+
+    if (len(inputX) <= 1 or len(inputY) <= 1):
+        [positive, output] = multiplication(inputX, positiveX, inputY, positiveY, b)
+        return [positive, output]
+    
+    inputXY = inputX, inputY
+    maxLen = max(len(inputX), len(inputY))
+
+    for digits in inputXY:
+        while len(digits) < maxLen:
+            digits.insert(0, 0)
+    
+    if (len(inputX) % 2 == 1):
+        inputX.insert(0, 0)
+
+    if (len(inputY) % 2 == 1):
+        inputY.insert(0, 0)
+    
+    n = max(len(inputX), len(inputY)) // 2
+
+    # define x_hi, x_lo, y_hi and y_lo
+    x_hi = inputX[:n]
+    x_lo = inputX[n:]
+    y_hi = inputY[:n]
+    y_lo = inputY[n:]
+
+    # calculate all multiplications with recursion
+    xy_hi = karatsuba(x_hi, True, y_hi, True, b)[1]
+    xy_lo = karatsuba(x_lo, True, y_lo, True, b)[1]
+    sumX = addition(x_hi, True, x_lo, True, b)[1]
+    sumY = addition(y_hi, True, y_lo, True, b)[1]
+    sumXY = karatsuba(sumX, True, sumY, True, b)[1]
+    [xy_positive, xy_temp] = subtraction(sumXY, True, xy_hi, True, b)
+    [positiveMix, xy_mix] = subtraction(xy_temp, xy_positive, xy_lo, True, b)
+    output = [0] * n * 10
+
+    for i in range(1, len(xy_lo) + 1):
+        output[-i] += xy_lo[-i]
+    
+    for i in range(1, len(xy_mix) + 1):
+        if positiveMix:
+            output[-(i + n)] += xy_mix[-i]
+        else:
+            output[-(i + n)] -= xy_mix[-i]
+
+    for i in range(1, len(xy_hi) + 1):
+        output[-(i + 2 * n)] += xy_hi[-i]
+
+    for i in range(1, len(output) + 1):
+        rem = 0
+        if (output[-i] >= b):
+            carry = output[-i] // b
+            output[-i] = output[-i] % b
+            output[-(i + 1)] += carry
+        elif (output[-i] < 0):
+            output[-(i + 1)] -= 1
+            rem = output[-i] + b
+            output[-i] = rem
+
+    i = 0
+    while i < (len(output) - 1):
+        if (output[i] == 0):
+            output.pop(0)
+            continue
+        break
+
+    return [positiveOutput, output]
+
+
+def euclidean(inputX, positiveX, inputY, positiveY, b):
+    x1, x2, y1, y2 = [True, [1]], [True, [0]], [True, [0]], [True, [1]]
+    eqZero = False
+    numberX, numberY = inputX.copy(), inputY.copy()
+
+    while not eqZero:
+        [r, q] = modular_reduction(numberX, True, numberY, b)[1:3]
+        numberX, numberY = numberY, r
+        qx2 = karatsuba(q, True, x2[1], x2[0], b)
+        qy2 = karatsuba(q, True, y2[1], y2[0], b)
+        x3 = subtraction(x1[1], x1[0], qx2[1], qx2[0], b)
+        y3 = subtraction(y1[1], y1[0], qy2[1], qy2[0], b)
+        x1, y1 = x2, y2
+        x2, y2 = x3, y3
+
+        i = 0;
+        while i < (len(numberY)-1):
+            if (numberY[i] == 0):
+                numberY.pop(0)
+                continue
+            break
+
+        if (numberY[0] == 0):
+            eqZero = True
+    
+    gcd = numberX
+
+    if positiveX:
+        x = x1
+    else:
+        x = [not x1[0], x1[1]]
+
+    if positiveY:
+        y = y1
+    else:
+        y = [not y1[0], y1[1]]
+    
+    return [gcd, x, y]
+
+
+def modular_reduction(inputX, positiveX, m, b):
+    # len of input X and len of mod
+    lenX = len(inputX)
+    lenM = len(m)
+    difLen = lenX - lenM 
+    coefficient = [0]*lenX
+    positive = True
+    eqZero = False
+    output = inputX.copy()
+
+    # step 2
+    for i in range(0, difLen + 1):
+        positive = True
+        coeffCounter = -1
+        n = difLen-i
+        tempX = output.copy()
+        modBase = m.copy()
+        for j in range(n):
+            modBase.append(0)
+        while positive:
+            coeffCounter += 1
+            output = tempX.copy()
+            [positive, tempX] = subtraction(output, True, modBase, True, b) #pak de positive output ding
+        coefficient[-(n+1)] = coeffCounter
+
+    i = 0;
+    while i < (len(output)-1):
+        if (output[i] == 0):
+            output.pop(0)
+            continue
+        break
+
+    if (output[0] == 0):
+        eqZero = True
+
+    if (positiveX or eqZero):
+        return [True, output, coefficient]
+    else: 
+        return [True, subtraction(m, True, output, True, b)[1], coefficient]
+
+
+def modular_addition(inputX, positiveX, inputY, positiveY, m, b):
+    [positive, sumXY] = addition(inputX, positiveX, inputY, positiveY, b)
+    output = modular_reduction(sumXY, positive, m, b)[0:2]
+    return output
+
+
+def modular_subtraction(inputX, positiveX, inputY, positiveY, m, b):
+    [positive, diffXY] = subtraction(inputX, positiveX, inputY, positiveY, b)
+    output = modular_reduction(diffXY, positive, m, b)[0:2]
+    return output
+    
+
+def modular_multiplication(inputX, positiveX, inputY, positiveY, m, b):
+    [positive, prodXY] = multiplication(inputX, positiveX, inputY, positiveY, b)
+    output = modular_reduction(prodXY, positive, m, b)[0:2]
+    return output
+
+
+def modular_inversion(inputX, positiveX, m , b): 
+    a = modular_reduction(inputX, positiveX, m, b)[1]
+    [gcd, x] = euclidean(a, True, m, True, b)[0:2]
+
+    if (gcd == [1]):
+        return modular_reduction(x[1], x[0], m, b)[0:2]
+    else:
+        return "inverse does not exist"
+
+print(modular_inversion([3],True,[2,7, 1],10))

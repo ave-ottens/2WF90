59255589a510ae7d0ecf0bc0fcaebedda9287c2d
jottens <j.m.a.ottens@student.tue.nl>
2019-09-30 12:16:24 +0200

    iuwhegoifsjerlgfjaowpieg

---
+++ Assignment 2/fields.py
@@ -0,0 +1,32 @@
+def add_table():
+    return
+
+def mult_table():
+    return
+
+def display_field():
+    return
+
+def add_field():
+    return
+
+def subtract_field():
+    return
+
+def multiply_field():
+    return
+
+def inverse_field():
+    return
+
+def division_field():
+    return
+
+def equals_field():
+    return
+
+def primitive():
+    return
+
+def find_primitive():
+    return

18157c5aa4d01f99df10a92cc21754c0a5ca007e
smartiej <54936446+smartiej@users.noreply.github.com>
2019-10-02 20:19:07 +0200

    Added add_poly, subtract_poly, and multiply_poly

--- Assignment 2/poly.py
+++ Assignment 2/poly.py
@@ -47,13 +47,57 @@ def deg_poly(mod, f):
     return 0 # non-zero coefficient could not be found
 
 def add_poly(mod, f, g):
-    return # list of coefficients, highest degree first
+    listFG = f, g    
+    maxLen = max(map(len, listFG))
+        
+    # add digits
+    for digits in listFG:
+        while len(digits) < maxLen:
+            digits.insert(0, 0)
+
+    # add element of f with index i with element of g with index i 
+    sumPoly = [sum(x) for x in zip(*listFG)]
+    output = display_poly(mod, sumPoly)
+
+    return output
 
 def subtract_poly(mod, f, g):
-    return # list of coefficients, highest degree first
+    listFG = f, g
+    maxLen = max(map(len, listFG))
+        
+    # add digits
+    for digits in listFG:
+        while len(digits) < maxLen:
+            digits.insert(0, 0) 
+
+    # difference between the digits from f with g according to index in each list
+    diffFG = [0] * maxLen
+    for i in range(maxLen):
+        diffFG[i] = f[i] - g[i]
+    output = display_poly(mod, diffFG)
+
+    return output
 
 def multiply_poly(mod, f, g):
-    return # list of coefficients, highest degree first
+    listFG = f, g    
+    maxLen = max(map(len, listFG))
+        
+    for digits in listFG:
+        while len(digits) < maxLen:
+            digits.insert(0, 0)
+
+    multFG = [0]*2*maxLen
+
+    for i in range(0, maxLen):
+        digit2 = g[-(i+1)]
+        for j in range(0, maxLen):
+            digit1 = f[-(j+1)]
+            outputNumber = digit1 * digit2
+            multFG[-(1+i+j)] += outputNumber
+
+    output = display_poly(mod, multFG)
+  
+    return output
 
 def long_div_poly(mod, f, g):
     q = None
